b0c9ce3cab5f4c756df62346f974e880ec17504d
AmrMohamad <amr.mohamed124124@gmail.com>
2023-09-13 15:41:20 +0300

    Add Models - Playlist - AudioTrack - Artist - UserProfile

---
+++ Spotify-iOS16/Models/Artist.swift
@@ -0,0 +1,8 @@
+//
+//  Artist.swift
+//  Spotify-iOS16
+//
+//  Created by Amr Mohamad on 13/09/2023.
+//
+
+import Foundation
---
+++ Spotify-iOS16/Models/UserProfile.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfile.swift
+//  Spotify-iOS16
+//
+//  Created by Amr Mohamad on 13/09/2023.
+//
+
+import Foundation
