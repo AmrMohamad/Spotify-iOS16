ac9c0711b6299a57ae3cc674bedb95c38740ffe5
AmrMohamad <amr.mohamed124124@gmail.com>
2023-09-14 19:45:34 +0300

    Prepare AuthManager

--- Spotify-iOS16/Controllers/Core/TabBarViewController.swift
+++ Spotify-iOS16/Controllers/Core/TabBarViewController.swift
@@ -13,16 +13,22 @@ class TabBarViewController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        /// a reference of HomeViewController
         let vc1 = HomeViewController()
+        /// a reference of SearchViewController
         let vc2 = SearchViewController()
+        /// a reference of LibraryViewController
         let vc3 = LibraryViewController()
         
         vc1.navigationItem.largeTitleDisplayMode = .always
         vc2.navigationItem.largeTitleDisplayMode = .always
         vc3.navigationItem.largeTitleDisplayMode = .always
         
+        /// a start point of UINavigation for HomeViewController
         let nav1 = UINavigationController(rootViewController: vc1)
+        /// a start point of UINavigation for SearchViewController
         let nav2 = UINavigationController(rootViewController: vc2)
+        /// a start point of UINavigation for LibraryViewController
         let nav3 = UINavigationController(rootViewController: vc3)
         
         nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
@@ -33,6 +39,7 @@ class TabBarViewController: UITabBarController {
         nav2.navigationBar.prefersLargeTitles = true
         nav3.navigationBar.prefersLargeTitles = true
         
+        // link ViewControllers to tabBar
         setViewControllers([nav1,nav2,nav3], animated: false)
     }
     
